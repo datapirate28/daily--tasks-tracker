8771d867cd25e3581873fb877ea0dbd2f35a39b4
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-07-18 03:14:29 +0300

    completed the app

---
+++ main.js
@@ -0,0 +1,41 @@
+const task = document.querySelector('#task');
+const tasks = document.querySelector('.tasks');
+
+
+document.querySelector('button').onclick = addTask
+
+function addTask(){
+    if(task.value === ''){
+        alert('Enter a task to proceed')
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = task.value
+        tasks.appendChild(li)
+        let span = document.createElement('span');
+        span.innerHTML = '\u00D7';
+        li.appendChild(span)
+        task.value = '';
+    }
+    saveData()
+}
+
+tasks.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData()
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+
+function saveData(){
+    localStorage.setItem('data', tasks.innerHTML);
+}
+
+function showTasks(){
+    tasks.innerHTML = localStorage.getItem('data');
+}
+
+showTasks();
+
